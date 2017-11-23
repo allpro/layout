29142896ed1c301e2396c0a675241f8b99374bcd
Robert Bar <robert@rbsoftware.pl>
2017-11-23 13:28:42 +0100

    remove the breaking line of code related to a removed .selector method in jQuery 3.x

--- source/stable/jquery.layout_and_plugins.js
+++ source/stable/jquery.layout_and_plugins.js
@@ -1829,7 +1829,7 @@ $.fn.layout = function (opts) {
 		,	$parent, n
 		;
 		// sC = state.container
-		sC.selector = $N.selector.split(".slice")[0];
+		// sC.selector = $N.selector.split(".slice")[0];
 		sC.ref		= (o.name ? o.name +' layout / ' : '') + tag + (id ? "#"+id : cls ? '.['+cls+']' : ''); // used in messages
 		sC.isBody	= (tag === "BODY");
 
